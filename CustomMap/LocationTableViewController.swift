646fb36d7cb8c74f407feb86d7b8dde816a49a86
HUONG <lamquynhhuong1304@gmail.com>
2019-12-13 00:42:25 +0700

    table annotation

--- CustomMap/LocationTableViewController.swift
+++ CustomMap/LocationTableViewController.swift
@@ -16,89 +16,88 @@ struct Location {
     var subTitle: String = ""
     
 }
-class LocationTableViewController: UITableViewController {
+class LocationTableViewController: UITableViewController, UISearchResultsUpdating {
     
-    let nowZone = Location(lat: 10.762932, long: 106.682182, title: "University Of Science", subTitle: "227 Đường Nguyễn Văn Cừ, Phường 4, Quận 5, Hồ Chí Minh, Việt Nam")
-    let hcmus = Location(lat: 10.764090, long: 106.681876, title: "Le Hong Phong High School", subTitle: "235 Đường Nguyễn Văn Cừ, Phường 4, Quận 5, Hồ Chí Minh, Việt Nam")
-    let lhp = Location(lat: 10.764337, long: 106.682649, title: "Now Zone", subTitle: "235 Đường Nguyễn Văn Cừ, Phường Nguyễn Cư Trinh, Quận 1, Hồ Chí Minh, Việt Nam")
+   
+    var selectedLocation: String = ""
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filterSearchBar.removeAll(keepingCapacity: false)
+        
+        let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text!)
+        let array = (listLocation as NSArray).filtered(using: searchPredicate)
+        filterSearchBar = array as! [String]
+        
+
+           self.tableView.reloadData()
+    }
+    
+    
+   // var matchingItems:[MKMapItem] = []
+    var mapView: MKMapView? = nil
+   
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       // LocationTableViewController
-        
-        //tableView.append(hcmus)
-        //tableView.append()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+      /*let nowZone = Location(lat: 10.762932, long: 106.682182, title: "University Of Science", subTitle: "227 Đường Nguyễn Văn Cừ, Phường 4, Quận 5, Hồ Chí Minh, Việt Nam")
+      let hcmus = Location(lat: 10.764090, long: 106.681876, title: "Le Hong Phong High School", subTitle: "235 Đường Nguyễn Văn Cừ, Phường 4, Quận 5, Hồ Chí Minh, Việt Nam")
+      let lhp = Location(lat: 10.764337, long: 106.682649, title: "Now Zone", subTitle: "235 Đường Nguyễn Văn Cừ, Phường Nguyễn Cư Trinh, Quận 1, Hồ Chí Minh, Việt Nam")*/
+    
+        listLocation.append("Now Zone")
+        listLocation.append("Van Hanh Mall")
+        listLocation.append("Tao Dan Park")
+        listLocation.append("Nguyen Hue Street")
+        listLocation.append("University of Science HCMC")
+        listLocation.append("Ba Na Hill")
+        listLocation.append("Hollywood")
+        tableView.reloadData()
     }
-
+  
+   
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-       return 0
-    }
+       if (isSearch){
+            return filterSearchBar.count
+        }
+        else {
+         return listLocation.count
+        }
+       
 
+    }
     
+  
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-       // cell.textLabel?.text =
-
-        return cell
-    }
+          let cell = tableView.dequeueReusableCell(withIdentifier: "cellLocation", for: indexPath)
+          if (isSearch){
+            cell.textLabel?.text = filterSearchBar[indexPath.row]
+            }
+        else
+        {
+            let selectedItem = listLocation[indexPath.row]
+                     cell.textLabel?.text = selectedItem
+        }
+         
+          return cell
+        }
     
+    
+    override func tableView(_ tableView: UITableView, didHighlightRowAt indexPath: IndexPath) {
+          let index = indexPath.row
+          selectedLocation = listLocation[index]
+          performSegue(withIdentifier: "unwindToMapSegue", sender: self)
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+      }
+    
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-}
+  
--- CustomMap/MapViewController.swift
+++ CustomMap/MapViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 import MapKit
 
 
+var listLocation : [String] = []
+var filterSearchBar = [String]()
+var searchController: UISearchController!
+var isSearch = false
+
 class Annotation: NSObject, MKAnnotation{
 let coordinate: CLLocationCoordinate2D
 let title: String?
@@ -21,120 +26,119 @@ init(latitude: CLLocationDegrees, longtitue: CLLocationDegrees, title: String?, 
     }
 }
 
+
+
 class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UISearchBarDelegate {
+
+ 
+    
+    // Annotation
+    var lat : Double = 0.0
+    var long :  Double = 0.0
+    var titleAnnotation : String = "abc"
+    var subTitleAnnotation : String = ""
+    
     // search
-    var searchController: UISearchController!
+ 
     var localSearchRequest: MKLocalSearch.Request!
     var localSearch: MKLocalSearch!
     var localSearchResponse: MKLocalSearch.Response!
     
     var annotation: MKAnnotation!
+    
+    // user's location
     var locationManager: CLLocationManager!
-   var isCurrentLocation: Bool = false
+    var isCurrentLocation: Bool = false
+    
     
     var activityIndicator: UIActivityIndicatorView!
 
     @IBOutlet weak var mapView: MKMapView!
 
    
-    var  currentCoordinate = CLLocationCoordinate2D()
+  //  var  currentCoordinate = CLLocationCoordinate2D()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        display(lat: 10.762932, long: 106.682182, title: "University Of Science", subTitle: "227 Đường Nguyễn Văn Cừ, Phường 4, Quận 5, Hồ Chí Minh, Việt Nam")
-        display(lat: 10.764090, long: 106.681876, title: "Le Hong Phong High School", subTitle: "235 Đường Nguyễn Văn Cừ, Phường 4, Quận 5, Hồ Chí Minh, Việt Nam")
-        display(lat: 10.764337, long: 106.682649, title: "Now Zone", subTitle: "235 Đường Nguyễn Văn Cừ, Phường Nguyễn Cư Trinh, Quận 1, Hồ Chí Minh, Việt Nam")
-        //configureLocationServices()
+        
+        
+        // display annotation
+//        display(lat: 10.762932, long: 106.682182, title: "University Of Science", subTitle: "227 Đường Nguyễn Văn Cừ, Phường 4, Quận 5, Hồ Chí Minh, Việt Nam")
+//        display(lat: 10.764090, long: 106.681876, title: "Le Hong Phong High School", subTitle: "235 Đường Nguyễn Văn Cừ, Phường 4, Quận 5, Hồ Chí Minh, Việt Nam")
+//        display(lat: 10.764337, long: 106.682649, title: "Now Zone", subTitle: "235 Đường Nguyễn Văn Cừ, Phường Nguyễn Cư Trinh, Quận 1, Hồ Chí Minh, Việt Nam")
+    
         // button get current location
         let currentLocationButton = UIBarButtonItem(title: "Your Location", style: UIBarButtonItem.Style.plain, target: self, action: #selector(MapViewController.currentLocationButtonAction(_:)))
         self.navigationItem.leftBarButtonItem = currentLocationButton
         
-        // search
+        // search location button
         let searchButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.search, target: self, action: #selector(MapViewController.searchButtonAction(_:)))
         self.navigationItem.rightBarButtonItem = searchButton
         
         mapView.delegate = self
-        
         activityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.gray)
         activityIndicator.hidesWhenStopped = true
         self.view.addSubview(activityIndicator)
-        
-       // zoomToLastestLocal(with: currentCoordinate)
-    
-        // Do any additional setup after loading the view.
+        userCurrentLocation()
     }
     
-    /*func configureLocationServices(){
-        locationManager.delegate = self
-        let status = CLLocationManager.authorizationStatus()
-        if (status == .notDetermined){
-            
-            locationManager.requestAlwaysAuthorization()
-        }
-        else if (status == .authorizedAlways || status == .authorizedWhenInUse ){
-           beginLocal(locationManager: locationManager)
-        }
-            
-    }*/
+ 
     
     
-    @objc func currentLocationButtonAction(_ sender: UIBarButtonItem) {
+    //user's current location
+   @objc func currentLocationButtonAction(_ sender: UIBarButtonItem) {
+       userCurrentLocation()
+    }
+    
+    func userCurrentLocation(){
         if (CLLocationManager.locationServicesEnabled()) {
-            if locationManager == nil {
-                locationManager = CLLocationManager()
-            }
-            locationManager?.requestWhenInUseAuthorization()
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest
-            locationManager.requestAlwaysAuthorization()
-            locationManager.startUpdatingLocation()
-            isCurrentLocation = true
-        }
+                   if locationManager == nil {
+                       locationManager = CLLocationManager()
+                   }
+                   locationManager?.requestWhenInUseAuthorization()
+                   locationManager.delegate = self
+                   locationManager.desiredAccuracy = kCLLocationAccuracyBest
+                   locationManager.requestAlwaysAuthorization()
+                   locationManager.startUpdatingLocation()
+                   isCurrentLocation = true
+               }
     }
+        
+    
+    // search button
     @objc func searchButtonAction(_ button: UIBarButtonItem) {
-    if searchController == nil {
-        searchController = UISearchController(searchResultsController: nil)
-    }
+        
+    let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationTableViewController") as! LocationTableViewController
+        searchController?.searchResultsUpdater = locationSearchTable as! UISearchResultsUpdating
+       locationSearchTable.mapView = mapView
+        searchController = UISearchController(searchResultsController: locationSearchTable.self)
     searchController.hidesNavigationBarDuringPresentation = false
-    self.searchController.searchBar.delegate = self
+    searchController.searchBar.delegate = self
     present(searchController, animated: true, completion: nil)
     }
     
     
+    
+    // search location
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //hide bar
         searchBar.resignFirstResponder()
         dismiss(animated: true, completion: nil)
-        
-        if self.mapView.annotations.count != 0 {
-            annotation = self.mapView.annotations[0]
-            self.mapView.removeAnnotation(annotation)
-        }
-        
-        localSearchRequest = MKLocalSearch.Request()
-        localSearchRequest.naturalLanguageQuery = searchBar.text
-        localSearch = MKLocalSearch(request: localSearchRequest)
-        localSearch.start { [weak self] (localSearchResponse, error) -> Void in
-            
-            if localSearchResponse == nil {
-                let alert = UIAlertView(title: nil, message: "Place not found", delegate: self, cancelButtonTitle: "Try again")
-                alert.show()
-                return
-            }
-            
-            let pointAnnotation = MKPointAnnotation()
-            pointAnnotation.title = searchBar.text
-            pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: localSearchResponse!.boundingRegion.center.latitude, longitude: localSearchResponse!.boundingRegion.center.longitude)
-            
-            let pinAnnotationView = MKPinAnnotationView(annotation: pointAnnotation, reuseIdentifier: nil)
-            self!.mapView.centerCoordinate = pointAnnotation.coordinate
-            self!.mapView.addAnnotation(pinAnnotationView.annotation!)
-        }
+        displaySearch(title: searchBar.text ?? "")
     }
+    
+    
+  
+    
     func display(lat : CLLocationDegrees, long: CLLocationDegrees, title: String? = nil , subTitle: String? = nil){
         let annotation = Annotation(latitude: lat, longtitue: long, title: title, subtitle: subTitle)
             mapView.addAnnotation(annotation)
             //mapView.setRegion(annotation.region, animated: true)
         }
+    
+    
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
         if !isCurrentLocation {
@@ -146,8 +150,9 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         let location = locations.last
         let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        
         self.mapView.setRegion(region, animated: true)
+       
+        
         if self.mapView.annotations.count != 0 {
             annotation = self.mapView.annotations[0]
             self.mapView.removeAnnotation(annotation)
@@ -158,56 +163,40 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         pointAnnotation.title = ""
         mapView.addAnnotation(pointAnnotation)
     }
-}
+    @IBAction func unwindToMapView(segue: UIStoryboardSegue) {
+        if let vc = segue.source as? LocationTableViewController{
+            titleAnnotation = vc.selectedLocation
+            displaySearch(title: titleAnnotation)
+        }
+    }
+    func displaySearch(title: String){
+        if self.mapView.annotations.count != 0 {
+            annotation = self.mapView.annotations[0]
+            self.mapView.removeAnnotation(annotation)
+        }
+        localSearchRequest = MKLocalSearch.Request()
+        localSearchRequest.naturalLanguageQuery = title
+        localSearch = MKLocalSearch(request: localSearchRequest)
+        localSearch.start { [weak self] (localSearchResponse, error) -> Void in
+            if (error != nil){
+                print(error?.localizedDescription)
+            }
+            else if localSearchResponse?.mapItems.count == 0 {
+                    print("Not found")
+                }
+                else {
+                    let pointAnnotation = MKPointAnnotation()
+                    pointAnnotation.title = self?.title
+                    pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: localSearchResponse!.boundingRegion.center.latitude,longitude: localSearchResponse!.boundingRegion.center.longitude)
 
+                    let pinAnnotationView = MKPinAnnotationView(annotation: pointAnnotation, reuseIdentifier: nil)
+                    self!.mapView.centerCoordinate = pointAnnotation.coordinate
+                    self!.mapView.addAnnotation(pinAnnotationView.annotation!)
+                   }
+        }
+    }
  
-
-    
-/*extension MapViewController: CLLocationManagerDelegate{
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        print("this status changed")
-        if (status == .authorizedAlways || status == .authorizedWhenInUse ){
-           beginLocal(locationManager: locationManager)
-        }
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-           print("Fail with error \(error)")
-       }
-       
-       func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-            print ("Did get lastest location")
-        guard let lastestLocation = locations.first else {return}
-        if currentCoordinate != nil{
-            zoomToLastestLocal(with: lastestLocation.coordinate)
-    }
-        //zoomToLastestLocal(with: lastestLocation.coordinate)
-        //addAnnotations()
-        
-        display(lat: 10.764337, long: 106.682649, title: "Now Zone", subTitle: "235 Đường Nguyễn Văn Cừ, Phường Nguyễn Cư Trinh, Quận 1, Hồ Chí Minh, Việt Nam")
-        display(lat: 10.762932, long: 106.682182, title: "University Of Science", subTitle: "227 Đường Nguyễn Văn Cừ, Phường 4, Quận 5, Hồ Chí Minh, Việt Nam")
-        display(lat: 10.764090, long: 106.681876, title: "Le Hong Phong High School", subTitle: "235 Đường Nguyễn Văn Cừ, Phường 4, Quận 5, Hồ Chí Minh, Việt Nam")
-        
-        currentCoordinate = lastestLocation.coordinate
-    }
-}*/
-
-
-/*extension MapViewController: MKMapViewDelegate{
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "AnnotationView")
-        if annotationView == nil {
-            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "AnnotationView")
-        }
-        return annotationView
-    }
-    
-    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
-        print("The annotation was selected  \(String(describing: view.annotation?.title))")
-    }
-
-}*/
-    
+}
 
 extension Annotation{
     var region : MKCoordinateRegion{
@@ -215,3 +204,5 @@ extension Annotation{
         return MKCoordinateRegion(center: coordinate, span: span)
     }
 }
+
+
