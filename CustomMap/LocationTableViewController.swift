8701b1a43b44d6bdca6ce27cd78cd8a81662951e
HUONG <lamquynhhuong1304@gmail.com>
2019-12-13 17:23:32 +0700

    add polyline

--- CustomMap/LocationTableViewController.swift
+++ CustomMap/LocationTableViewController.swift
@@ -9,19 +9,16 @@
 import UIKit
 import MapKit
 
-struct Location {
-    var lat: Double
-    var long: Double
-    var title: String = ""
-    var subTitle: String = ""
+
+class LocationTableViewController: UITableViewController, UISearchResultsUpdating, UISearchBarDelegate {
     
-}
-class LocationTableViewController: UITableViewController, UISearchResultsUpdating {
-    
-   
+   @IBOutlet weak var searchBar: UISearchBar!
+    var listLocation : [String] = []
+    var filterSearchBar = [String]()
     var selectedLocation: String = ""
+    var isSearch : Bool = false
     
-    func updateSearchResults(for searchController: UISearchController) {
+   func updateSearchResults(for searchController: UISearchController) {
         filterSearchBar.removeAll(keepingCapacity: false)
         
         let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text!)
@@ -29,14 +26,13 @@ class LocationTableViewController: UITableViewController, UISearchResultsUpdatin
         filterSearchBar = array as! [String]
         
 
-           self.tableView.reloadData()
+        self.tableView.reloadData()
     }
     
     
    // var matchingItems:[MKMapItem] = []
     var mapView: MKMapView? = nil
    
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,18 +40,61 @@ class LocationTableViewController: UITableViewController, UISearchResultsUpdatin
       let hcmus = Location(lat: 10.764090, long: 106.681876, title: "Le Hong Phong High School", subTitle: "235 Đường Nguyễn Văn Cừ, Phường 4, Quận 5, Hồ Chí Minh, Việt Nam")
       let lhp = Location(lat: 10.764337, long: 106.682649, title: "Now Zone", subTitle: "235 Đường Nguyễn Văn Cừ, Phường Nguyễn Cư Trinh, Quận 1, Hồ Chí Minh, Việt Nam")*/
     
-        listLocation.append("Now Zone")
+       listLocation.append("Now Zone")
         listLocation.append("Van Hanh Mall")
         listLocation.append("Tao Dan Park")
         listLocation.append("Nguyen Hue Street")
-        listLocation.append("University of Science HCMC")
+        listLocation.append("Khoa Hoc Tu Nhien")
+        listLocation.append ("Cong Truong Me Linh")
+        listLocation.append("Winter Palace")
         listLocation.append("Ba Na Hill")
-        listLocation.append("Hollywood")
+        listLocation.append("Hung Vuong Plaza")
         tableView.reloadData()
+        searchBar.delegate = self
+
     }
   
-   
     
+    @IBAction func cancelTapped(_ sender: Any) {
+        dismiss(animated: false, completion: nil)
+    }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        selectedLocation = searchBar.text!
+        performSegue(withIdentifier: "unwindToMapSegue", sender: self)
+    }
+   
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        isSearch = true;
+    }
+
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        isSearch = false;
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearch = false;
+    }
+
+    func searchBarBookmarkButtonClicked(_ searchBar: UISearchBar) {
+        isSearch = false;
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+
+        filterSearchBar = listLocation.filter({ (text) -> Bool in
+            let tmp: NSString = text as NSString
+            let range = tmp.range(of: searchText, options: NSString.CompareOptions.caseInsensitive)
+            return range.location != NSNotFound
+        })
+        if(filterSearchBar.count == 0){
+            isSearch = false;
+        } else {
+            isSearch = true;
+        }
+        self.tableView.reloadData()
+    }
+
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -72,8 +111,6 @@ class LocationTableViewController: UITableViewController, UISearchResultsUpdatin
         else {
          return listLocation.count
         }
-       
-
     }
     
   
@@ -85,16 +122,15 @@ class LocationTableViewController: UITableViewController, UISearchResultsUpdatin
         else
         {
             let selectedItem = listLocation[indexPath.row]
-                     cell.textLabel?.text = selectedItem
+            cell.textLabel?.text = selectedItem
         }
-         
-          return cell
-        }
-    
+        
+        return cell
+    }
     
     override func tableView(_ tableView: UITableView, didHighlightRowAt indexPath: IndexPath) {
           let index = indexPath.row
-          selectedLocation = listLocation[index]
+            selectedLocation = listLocation[index]
           performSegue(withIdentifier: "unwindToMapSegue", sender: self)
 
       }
--- CustomMap/MapViewController.swift
+++ CustomMap/MapViewController.swift
@@ -10,8 +10,7 @@ import UIKit
 import MapKit
 
 
-var listLocation : [String] = []
-var filterSearchBar = [String]()
+
 var searchController: UISearchController!
 var isSearch = false
 
@@ -29,8 +28,18 @@ init(latitude: CLLocationDegrees, longtitue: CLLocationDegrees, title: String?, 
 
 
 class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UISearchBarDelegate {
-
- 
+    // direction
+    @IBOutlet weak var getDirectionButton: UIButton!
+    var startPointTextField : UITextField?
+    var destinationTextField : UITextField?
+    var start : String = ""
+    var destination: String = ""
+    var startLatitude : Double = 0.0
+    var startLongtitude : Double = 0.0
+    var destinationLatitude : Double = 0.0
+    var destinationLongtitude: Double = 0.0
+    let sourceAnnotation = MKPointAnnotation()
+    let destinationAnnotation = MKPointAnnotation()
     
     // Annotation
     var lat : Double = 0.0
@@ -62,25 +71,30 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+
         // display annotation
-//        display(lat: 10.762932, long: 106.682182, title: "University Of Science", subTitle: "227 Đường Nguyễn Văn Cừ, Phường 4, Quận 5, Hồ Chí Minh, Việt Nam")
-//        display(lat: 10.764090, long: 106.681876, title: "Le Hong Phong High School", subTitle: "235 Đường Nguyễn Văn Cừ, Phường 4, Quận 5, Hồ Chí Minh, Việt Nam")
-//        display(lat: 10.764337, long: 106.682649, title: "Now Zone", subTitle: "235 Đường Nguyễn Văn Cừ, Phường Nguyễn Cư Trinh, Quận 1, Hồ Chí Minh, Việt Nam")
-    
+        display(lat: 10.762932, long: 106.682182, title: "University Of Science", subTitle: "227 Đường Nguyễn Văn Cừ, Phường 4, Quận 5, Hồ Chí Minh, Việt Nam")
+        display(lat: 10.764090, long: 106.681876, title: "Le Hong Phong High School", subTitle: "235 Đường Nguyễn Văn Cừ, Phường 4, Quận 5, Hồ Chí Minh, Việt Nam")
+        display(lat: 10.764337, long: 106.682649, title: "Now Zone", subTitle: "235 Đường Nguyễn Văn Cừ, Phường Nguyễn Cư Trinh, Quận 1, Hồ Chí Minh, Việt Nam")
+        display(lat: 10.295556, long: 105.766594, title: "My Home", subTitle: "119 Trần Hưng Đạo, SaDec, Đồng Tháp")
+        display(lat: 10.288616, long: 105.764067, title: "Bao's Home", subTitle: "324 Trần Hưng Đạo, SaDec, Đồng Tháp")
         // button get current location
         let currentLocationButton = UIBarButtonItem(title: "Your Location", style: UIBarButtonItem.Style.plain, target: self, action: #selector(MapViewController.currentLocationButtonAction(_:)))
         self.navigationItem.leftBarButtonItem = currentLocationButton
-        
         // search location button
-        let searchButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.search, target: self, action: #selector(MapViewController.searchButtonAction(_:)))
-        self.navigationItem.rightBarButtonItem = searchButton
+//        let searchButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.search, target: self, action: #selector(MapViewController.searchButtonAction(_:)))
+//        self.navigationItem.rightBarButtonItem = searchButton
         
         mapView.delegate = self
         activityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.gray)
         activityIndicator.hidesWhenStopped = true
         self.view.addSubview(activityIndicator)
         userCurrentLocation()
+        
+        //
+
+
+        
     }
     
  
@@ -92,6 +106,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     }
     
     func userCurrentLocation(){
+        removeOldDirect()
         if (CLLocationManager.locationServicesEnabled()) {
                    if locationManager == nil {
                        locationManager = CLLocationManager()
@@ -105,30 +120,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
                }
     }
         
-    
-    // search button
-    @objc func searchButtonAction(_ button: UIBarButtonItem) {
-        
-    let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationTableViewController") as! LocationTableViewController
-        searchController?.searchResultsUpdater = locationSearchTable as! UISearchResultsUpdating
-       locationSearchTable.mapView = mapView
-        searchController = UISearchController(searchResultsController: locationSearchTable.self)
-    searchController.hidesNavigationBarDuringPresentation = false
-    searchController.searchBar.delegate = self
-    present(searchController, animated: true, completion: nil)
-    }
-    
-    
-    
-    // search location
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        //hide bar
-        searchBar.resignFirstResponder()
-        dismiss(animated: true, completion: nil)
-        displaySearch(title: searchBar.text ?? "")
-    }
-    
-    
+ 
   
     
     func display(lat : CLLocationDegrees, long: CLLocationDegrees, title: String? = nil , subTitle: String? = nil){
@@ -137,8 +129,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
             //mapView.setRegion(annotation.region, animated: true)
         }
     
-    
-    
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
         if !isCurrentLocation {
@@ -167,6 +158,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         if let vc = segue.source as? LocationTableViewController{
             titleAnnotation = vc.selectedLocation
             displaySearch(title: titleAnnotation)
+            removeOldDirect()
         }
     }
     func displaySearch(title: String){
@@ -195,14 +187,197 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
                    }
         }
     }
- 
-}
-
-extension Annotation{
-    var region : MKCoordinateRegion{
-    let span = MKCoordinateSpan(latitudeDelta: 10.7628, longitudeDelta: 106.6683)
-        return MKCoordinateRegion(center: coordinate, span: span)
+    
+    @IBAction func getDirectionTapped(_ sender: Any) {
+         let cancelAction = UIAlertAction(title: "Cancel", style:.cancel, handler: nil)
+        let alert = UIAlertController(title: "Get Direction", message: "Choose starting point", preferredStyle: .alert)
+        let userLocation = UIAlertAction(title: "Your Location", style: .default, handler: self.startUserLocationTapped)
+        let anotherLocation = UIAlertAction(title: "Another Location", style: .default, handler: self.anotherLocationTapped)
+        alert.addAction(userLocation)
+        alert.addAction(anotherLocation)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: false)
     }
-}
+    
+    func startUserLocationTapped(alert: UIAlertAction){
+        let cancelAction = UIAlertAction(title: "Cancel", style:.cancel, handler: nil)
+        let get = UIAlertAction(title: "Go", style: .default, handler: self.getDirectionFromUserLocation)
+        let alert = UIAlertController(title: "Get Direction", message: "Start from your position", preferredStyle: .alert)
+        alert.addTextField { (destinationTextField) in
+            self.destinationPoint(textField: destinationTextField)
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(get)
+        self.present(alert, animated: false)
+        
+    }
+    func anotherLocationTapped(alert: UIAlertAction){
+        let cancelAction = UIAlertAction(title: "Cancel", style:.cancel, handler: nil)
+        let get = UIAlertAction(title: "Go", style: .default, handler: self.getDirecttionFromAnotherPoint)
+        let alert = UIAlertController(title: "Get Direction", message:"", preferredStyle: .alert)
+        alert.addTextField { (startPointTextField) in
+            self.startPoint(textField: startPointTextField)
+        }
+        alert.addTextField { (destinationTextField) in
+            self.destinationPoint(textField: destinationTextField)
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(get)
+        self.present(alert, animated: false)
+    }
+    func startPoint(textField: UITextField!){
+        startPointTextField = textField
+        startPointTextField?.placeholder = "Choose starting pointing..."
+        
+    }
+    
+    func destinationPoint(textField: UITextField!){
+      destinationTextField = textField
+       destinationTextField?.placeholder = "Choose destination..."
+        
+    }
+    func getDirectionFromUserLocation(alert: UIAlertAction!){
+        alert.isEnabled = true
+       if (CLLocationManager.locationServicesEnabled()) {
+           if locationManager == nil {
+               locationManager = CLLocationManager()
+           }
+           locationManager?.requestWhenInUseAuthorization()
+           locationManager.delegate = self
+         //  locationManager.desiredAccuracy = kCLLocationAccuracyBest
+           locationManager.requestAlwaysAuthorization()
+           locationManager.startUpdatingLocation()
+           //isCurrentLocation = true
+       }
+        startLatitude = (locationManager.location as AnyObject).coordinate.latitude
+        startLongtitude = (locationManager.location?.coordinate.longitude)!
+        
+        localSearchRequest = MKLocalSearch.Request()
+        let search = destinationTextField?.text
+        localSearchRequest.naturalLanguageQuery = search
+        localSearch = MKLocalSearch(request: localSearchRequest)
+        localSearch.start { [weak self] (localSearchResponse, error) -> Void in
+            if (error != nil){
+                print(error?.localizedDescription as Any)
+            }
+            else if localSearchResponse?.mapItems.count == 0 {
+                    print("Not found")
+                }
+                else {
+                self!.destinationLatitude = (localSearchResponse?.boundingRegion.center.latitude)!
+                self!.destinationLongtitude = (localSearchResponse?.boundingRegion.center.longitude)!
+                self!.removeOldDirect()
+                self!.getDirect(sourcelat: self!.startLatitude, sourcelong: self!.startLongtitude, destinationlat: self!.destinationLatitude, destinationlong: self!.destinationLongtitude)
+                }
+            }
+        }
+    
+    func getDirecttionFromAnotherPoint(alert: UIAlertAction){
+        
+        localSearchRequest = MKLocalSearch.Request()
+        var search = startPointTextField!.text
+        localSearchRequest.naturalLanguageQuery = search
+               localSearch = MKLocalSearch(request: localSearchRequest)
+               localSearch.start { [weak self] (localSearchResponse, error) -> Void in
+                   if (error != nil){
+                       print(error?.localizedDescription as Any)
+                   }
+                   else if localSearchResponse?.mapItems.count == 0 {
+                           print("Not found")
+                       }
+                       else {
+                    self!.startLatitude = (localSearchResponse?.boundingRegion.center.latitude)!
+                    self!.startLongtitude = (localSearchResponse?.boundingRegion.center.longitude)!
+                    search = self!.destinationTextField!.text
+                    self!.localSearchRequest.naturalLanguageQuery = search
+                    self!.localSearch = MKLocalSearch(request: self!.localSearchRequest)
+                    self!.localSearch.start { [weak self] (localSearchResponse, error) -> Void in
+                                      if (error != nil){
+                                          print(error?.localizedDescription as Any)
+                                      }
+                                      else if localSearchResponse?.mapItems.count == 0 {
+                                              print("Not found")
+                                          }
+                                          else {
+                                           self!.destinationLatitude = (localSearchResponse?.boundingRegion.center.latitude)!
+                                           self!.destinationLongtitude = (localSearchResponse?.boundingRegion.center.longitude)!
+                                            self!.removeOldDirect()
+                                           self!.getDirect(sourcelat: self!.startLatitude, sourcelong: self!.startLongtitude, destinationlat: self!.destinationLatitude, destinationlong: self!.destinationLongtitude)
+                                   }
+                           }
+                          
+                }
+        }
 
+        
+    }
+    
+    func getDirect(sourcelat: Double, sourcelong: Double, destinationlat: Double, destinationlong: Double){
+      
+        let sourceLocation = CLLocationCoordinate2D(latitude: sourcelat, longitude: sourcelong)
+        let destinationLocation = CLLocationCoordinate2D(latitude: destinationlat, longitude: destinationlong)
+        
+        let sourcePlacemark = MKPlacemark(coordinate: sourceLocation, addressDictionary: nil)
+        let destinationPlacemark = MKPlacemark(coordinate: destinationLocation, addressDictionary: nil)
+        
+        let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
+               let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+        
+       // let sourceAnnotation = MKPointAnnotation()
+        sourceAnnotation.title = "Starting point"
+        if let location = sourcePlacemark.location{
+            sourceAnnotation.coordinate = location.coordinate
+        }
+        //let destinationAnnotation = MKPointAnnotation()
+        destinationAnnotation.title = "Destination point"
+        if let location = destinationPlacemark.location{
+            destinationAnnotation.coordinate = location.coordinate
+        }
+        
+         self.mapView.showAnnotations([sourceAnnotation,destinationAnnotation], animated: true )
+        
+        let directionRequest = MKDirections.Request()
+        directionRequest.source = sourceMapItem
+        directionRequest.destination = destinationMapItem
+        directionRequest.transportType = .automobile
+        
+        let directions = MKDirections(request: directionRequest)
+        directions.calculate {
+            (response, error) -> Void in
+            
+            guard let response = response else {
+                if let error = error {
+                    print("Error: \(error)")
+                }
+                
+                return
+            }
+           
+            let route = response.routes[0]
+            self.mapView.addOverlay((route.polyline), level: MKOverlayLevel.aboveRoads)
+            
+            let rect = route.polyline.boundingMapRect
+            self.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
+        }
+    }
+    
+    func removeOldDirect(){
+        mapView.overlays.forEach {
+              if ($0 is MKPolyline) {
+                  self.mapView.removeOverlay($0)
+                   }
+               }
+        mapView.removeAnnotation(sourceAnnotation)
+        mapView.removeAnnotation(destinationAnnotation)
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+       
+            let polyLineRender = MKPolylineRenderer(overlay: overlay)
+            polyLineRender.strokeColor = UIColor.blue
+            polyLineRender.lineWidth = 4
+            return polyLineRender
+    }
+  
+}
 
